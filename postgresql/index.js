8af3c9c0c36dbdbc30e032d6d29f1d2916fa97ee
Lory Zhang <zhang.rouyun@gmail.com>
2018-02-10 17:05:30 -0800

    set up db for production

--- postgresql/index.js
+++ postgresql/index.js
@@ -3,17 +3,26 @@ const pg = require('pg');
 const { Pool } = pg;
 
 const pool = new Pool({
-  database: 'surgereactor',
+  database: 'logger',
   user: 'surgereactor',
-  password: 'surgereactor',
-  // port: 5432,
-  // host: 'localhost',
-  // ssl: true,
+  password: 'surge',
+  port: 5432,
+  host: 'ec2-54-200-168-147.us-west-2.compute.amazonaws.com',
   max: 100,
   min: 4,
   idleTimeoutMillis: 1000,
   connectionTimeoutMillis: 1000,
 });
+
+// const pool = new Pool({
+  // database: 'surgereactor',
+  // user: 'surgereactor',
+  // password: 'surgereactor',
+  // max: 100,
+  // min: 4,
+  // idleTimeoutMillis: 1000,
+  // connectionTimeoutMillis: 1000,
+// });
 
 
 module.exports = pool;
