6b0e2a18159e48f1a5def1d3a8de3b3a63987d49
EC2 Default User <ec2-user@ip-172-31-28-39.us-west-2.compute.internal>
2018-02-12 22:56:35 +0000

    tested after seperate server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const newRelic = require('newrelic');
-const consumer = require('../aws/consumer.js');
+//const consumer = require('../aws/consumer.js');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router.js');
-const job = require('./worker.js');
+//const job = require('./worker.js');
 
 const app = express();
 const port = process.env.port || 8080;
@@ -13,7 +13,7 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
-
+/*
 consumer.addSupply.on('error when poiling message from supply', (err) => {
   console.log(err.message);
 });
@@ -30,7 +30,7 @@ consumer.addRequest.start();
 
 job.start();
 console.log('job status', job.running);
-
+*/
 app.listen(port, () => {
   console.log (`listening to port ${port}`);
 });
